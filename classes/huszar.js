31b75c4e8b0d15220e82ad41bedd60dc6437aac6
MrErik402 <bujakierikattila@turr.hu>
2026-01-20 09:38:39 +0100

    ON-10 - Huszár és Vezér osztály létrehozása |TESZTRE VÁR|

--- classes/huszar.js
+++ classes/huszar.js
@@ -1,11 +1,11 @@
 /*Ide jön a huszár osztály, lépések, ütések, stb. */
 
 class Huszar {
-    canMoveInLShape = true; // Can move in an L-shape
-    canAttack = true; // Can attack on the L-shape squares
-    isBlocked = false; // If the huszár is blocked by another piece
-    color = "white" | "black";
-    faIcon = "fa-regular fa-chess-knight" | "fa-solid fa-chess-knight"; // Font Awesome icon for the huszár, white and black
+    canMoveInLShape = true; // L alakban tud lépni
+    canAttack = true; // L alakban tud ütni
+    isBlocked = false; // A huszár átugrik bábukat, ezért ez jellemzően nem használt
+    color = "white"; // "white" | "black"
+    faIcon = "fa-regular fa-chess-knight"; // fehér: fa-regular, fekete: fa-solid
 
     position = {
         x: 0,
@@ -15,5 +15,59 @@ class Huszar {
     constructor(color, position) {
         this.color = color;
         this.position = position;
+        this.faIcon =
+          color === "white"
+            ? "fa-regular fa-chess-knight"
+            : "fa-solid fa-chess-knight";
+    }
+
+    _inBounds(x, y) {
+        return x >= 0 && x < 8 && y >= 0 && y < 8;
+    }
+
+    /**
+     * board: 8x8 tömb (board[y][x]); üres: null; foglalt: pl. { type, color }.
+     * @param {Array<Array<any>>} board
+     * @returns {{moves:{x:number,y:number}[], attacks:{x:number,y:number}[]}}
+     */
+    getPossibleMoves(board) {
+        const moves = [];
+        const attacks = [];
+
+        const { x, y } = this.position;
+
+        // 8 lehetséges huszár ugrás (2+1)
+        const jumps = [
+            { dx: 2, dy: 1 },
+            { dx: 2, dy: -1 },
+            { dx: -2, dy: 1 },
+            { dx: -2, dy: -1 },
+            { dx: 1, dy: 2 },
+            { dx: 1, dy: -2 },
+            { dx: -1, dy: 2 },
+            { dx: -1, dy: -2 },
+        ];
+
+        for (const { dx, dy } of jumps) {
+            const tx = x + dx;
+            const ty = y + dy;
+            if (!this._inBounds(tx, ty)) continue;
+
+            const cell = board?.[ty]?.[tx];
+            if (cell == null) {
+                moves.push({ x: tx, y: ty });
+            } else if (cell.color && cell.color !== this.color) {
+                attacks.push({ x: tx, y: ty });
+            }
+        }
+
+        return { moves, attacks };
+    }
+
+    /**
+     * @param {{x:number,y:number}} to
+     */
+    moveTo(to) {
+        this.position = { x: to.x, y: to.y };
     }
 }
--- classes/vezer.js
+++ classes/vezer.js
@@ -1,13 +1,13 @@
 /*Ide jön a királynő/vezér osztály, lépések, ütések, stb. */
 
 class Vezer {
-    canMoveDiagonally = true; // Can move diagonally
-    canAttack = true; // Can attack on the diagonal and horizontal and vertical squares
-    canMoveHorizontally = true; // Can move horizontally
-    canMoveVertically = true; // Can move vertically
-    isBlocked = false; // If the vezér is blocked by another piece
-    color = "white" | "black";
-    faIcon = "fa-regular fa-chess-queen" | "fa-solid fa-chess-queen"; // Font Awesome icon for the vezér, white and black
+    canMoveDiagonally = true; // Átlósan tud lépni
+    canAttack = true; // Átlósan + vízszintesen + függőlegesen tud ütni
+    canMoveHorizontally = true; // Vízszintesen tud lépni
+    canMoveVertically = true; // Függőlegesen tud lépni
+    isBlocked = false; // Akkor igaz, ha egy bábu “blokkolja” az útját
+    color = "white"; // "white" | "black"
+    faIcon = "fa-regular fa-chess-queen"; // fehér: fa-regular, fekete: fa-solid
     position = {
         x: 0,
         y: 0,
@@ -16,5 +16,68 @@ class Vezer {
     constructor(color, position) {
         this.color = color;
         this.position = position;
+        this.faIcon =
+          color === "white"
+            ? "fa-regular fa-chess-queen"
+            : "fa-solid fa-chess-queen";
+    }
+
+    _inBounds(x, y) {
+        return x >= 0 && x < 8 && y >= 0 && y < 8;
+    }
+
+    /**
+     * board: 8x8 tömb (board[y][x]); üres: null; foglalt: pl. { type, color }.
+     * @param {Array<Array<any>>} board
+     * @returns {{moves:{x:number,y:number}[], attacks:{x:number,y:number}[]}}
+     */
+    getPossibleMoves(board) {
+        const moves = [];
+        const attacks = [];
+
+        const { x, y } = this.position;
+
+        // 8 irány: vízszintes, függőleges, átlók (akadályig)
+        const directions = [
+            { dx: 1, dy: 0 },
+            { dx: -1, dy: 0 },
+            { dx: 0, dy: 1 },
+            { dx: 0, dy: -1 },
+            { dx: 1, dy: 1 },
+            { dx: -1, dy: 1 },
+            { dx: 1, dy: -1 },
+            { dx: -1, dy: -1 },
+        ];
+
+        for (const { dx, dy } of directions) {
+            let cx = x + dx;
+            let cy = y + dy;
+
+            while (this._inBounds(cx, cy)) {
+                const cell = board?.[cy]?.[cx];
+
+                if (cell == null) {
+                    moves.push({ x: cx, y: cy });
+                } else {
+                    // Saját bábu megállítja, ellenfélre lehet ütni és megáll
+                    if (cell.color && cell.color !== this.color) {
+                        attacks.push({ x: cx, y: cy });
+                    }
+                    break;
+                }
+
+                cx += dx;
+                cy += dy;
+            }
+        }
+
+        return { moves, attacks };
+    }
+
+    /**
+     * @param {{x:number,y:number}} to
+     */
+    moveTo(to) {
+        this.position = { x: to.x, y: to.y };
     }
 }
